042c9f6477371c400c7d7a194dd3b55589cbcc99
Mahendra Kariya <mahendrakariya@gmail.com>
2014-04-01 11:50:28 +0530

    mahendra, simij | #37 | fixing template url

--- src/multiselect/multiselect.js
+++ src/multiselect/multiselect.js
@@ -57,7 +57,7 @@ multiselectModule.directive('multiselect', function() {
             leftList: "=",
             rightList: "="
         },
-        templateUrl: 'js/lib/angular-multiselect/template/multiselect.html',
+        templateUrl: 'js/lib/angularjs-directives/template/multiselect/multiselect.html',
         replace: true,
         controller: 'MultiSelectController'
     };
