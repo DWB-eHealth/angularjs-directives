2c3141e418ba26eebb8214fb3988680fb99a76bb
Mansi Singhal <msinghal@thoughtworks.com>
2014-05-05 14:58:00 +0530

    Revert "Mansi,Aravind | Fixing the directive."
    This reverts commit 3cbab73f877dd493e2d5519d4792b0d01e168f4b.

--- src/notIn/notIn.js
+++ src/notIn/notIn.js
@@ -1,38 +1,26 @@
-var equalsModule = angular.module('ui.notIn', []);
+var notInModule = angular.module('ui.notIn', []);
 
-equalsModule.directive('notIn', function() {
+notInModule.directive('notIn', function() {
     return {
         restrict: 'A',
         require: 'ngModel',
+        scope: {
+            notIn: '=',
+        },
         link: function(scope, elm, attrs, ctrl) {
             var thisValue;
             var otherValues;
 
-            attrs.$observe('notIn', function(values) {
-                otherValues = scope.$eval(values);
-                validate();
-            });
-
             ctrl.$parsers.unshift(function(viewValue) {
-                thisValue = viewValue.toLowerCase();
-                validate();
-                return thisValue;
+                isValid = doesNotContain(viewValue, scope.notIn);
+                ctrl.$setValidity('isWithin', isValid);
+                return viewValue;
             });
 
-            var any = function(arr, predicate) {
-                for (var e in arr) {
-                    if (predicate(arr[e])) {
-                        return true;
-                    }
-                }
-                return false;
-            };
-
-            var validate = function() {
-                var isWithin = any(otherValues, function(value) {
-                    return value && thisValue && (value.toLowerCase() === thisValue.toLowerCase());
+            var doesNotContain = function(needle, haystack) {
+                return !haystack.some(function(value) {
+                    return value.toLowerCase() === needle.toLowerCase();
                 });
-                ctrl.$setValidity('isWithin', !isWithin);
             };
         },
     };
--- src/notIn/test/notIn.spec.js
+++ src/notIn/test/notIn.spec.js
@@ -3,35 +3,43 @@ describe('notIn', function() {
 
     beforeEach(module('ui.notIn'));
     beforeEach(inject(function($rootScope, $compile) {
-        $scope = $rootScope.$new();
         compile = $compile;
+
+        $scope = $rootScope.$new();
+        $scope.existingProjects = ["prj A", "prj B", "prj C"];
 
         var element = angular.element(
             '<form name="form">' +
-            '<input type="text" name="projectName" id="projectName" ng-model="project.name" required not-in="{{existingProjects}}"/>' +
+            '<input type="text" name="projectName" id="projectName" ng-model="project.name" not-in="existingProjects">' +
             '</form>'
         );
 
         compile(element)($scope);
-        $scope.existingProjects = ["prj A", "prj B", "prj C"];
-        $scope.$apply();
+        $scope.$digest();
         form = $scope.form;
     }));
 
-    it('should invalidate if value present in list', function() {
+    it('should set invalid to true if existingProjects contains specified value', function() {
         form.projectName.$setViewValue('PRJ a');
-        $scope.$apply();
+        $scope.$digest();
 
         expect(form.projectName.$invalid).toBe(true);
         expect(form.projectName.$error.isWithin).toBe(true);
     });
 
-    it('should validate if value not present in list', function() {
+    it('should set invalid to false if existingProjects does not contain specified value', function() {
         form.projectName.$setViewValue('prj D');
-        $scope.$apply();
+        $scope.$digest();
 
         expect(form.projectName.$invalid).toBe(false);
         expect(form.projectName.$error.isWithin).toBe(false);
+    });
+
+    it('should set invalid to false if no value is specified', function() {
+        $scope.$digest();
+
+        expect(form.projectName.$invalid).toBe(false);
+        expect(form.projectName.$error.isWithin).toBe(undefined);
     });
 
 });
